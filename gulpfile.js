0ec86f1db6e5168a46067d75e46ed4176ee8e13d
Defolto <37343435+Defolto@users.noreply.github.com>
2020-01-21 11:12:57 +0300

    add stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const gulpStylelint = require('gulp-stylelint');
 
 function style(){
     return gulp.src('./scss/**/*.scss')
@@ -8,6 +9,16 @@ function style(){
             .pipe(gulp.dest('./css'))
             .pipe(browserSync.stream());
 }
+
+function lintCss(){
+    return gulp.src('./scss/**/*.scss')
+        .pipe(gulpStylelint({
+            reporters: [
+            {formatter: 'string', console: true}
+            ]
+        }));
+}
+
 
 function watch(){
     browserSync.init({
@@ -20,4 +31,5 @@ function watch(){
 }
 
 exports.style = style;
+exports.lintCss = lintCss;
 exports.watch = watch;
